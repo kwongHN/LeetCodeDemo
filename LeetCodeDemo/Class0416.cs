8c9fc3618094dd788627e6ae333df81f9b152912
kuangdunli <270120184@qq.com>
2021-04-18 00:31:54 +0800

    vs与git联合使用

--- LeetCodeDemo/Class0416.cs
+++ LeetCodeDemo/Class0416.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace LeetCodeDemo
 {
     class Class0416
-    {
+    { 
+        //这是第二次测试提交gitHub
         /*
          * 
          * 给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
