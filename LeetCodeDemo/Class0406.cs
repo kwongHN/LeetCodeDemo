7fa758d5567f6d099901cc6d58dba88074e207a0
kuangdunli <270120184@qq.com>
2021-04-18 01:33:42 +0800

    小乌龟提交代码

--- LeetCodeDemo/Class0406.cs
+++ LeetCodeDemo/Class0406.cs
@@ -8,7 +8,7 @@ namespace LeetCodeDemo
 {
     class Class0406
     {
-        /*测试同步  ddqdq*/
+        /*测试同步  ddqdq wdqw*/
         /*
          * 请你设计一个可以解释字符串 command 的 Goal 解析器 。command 由 "G"、"()" 和/或 "(al)" 按某种顺序组成。Goal 解析器会将 "G" 解释为字符串 "G"、"()" 解释为字符串 "o" ，"(al)" 解释为字符串 "al" 。然后，按原顺序将经解释得到的字符串连接成一个字符串。
 
